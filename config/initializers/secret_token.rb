d065a3765689bc498ccd05269013a2c568a0ebbe
Ben Hazelwood <ben@parkgrades.com>
2013-08-10 13:41:34 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtApp::Application.config.secret_token = '1e17755e8208dbd4ffee72e3dc20c03a54eaa08f218f9c27afaaa0f3223e5350bba3e1063323c6eec3683759ba3b0d5b3b92854d074f7c177cb51890c9b727e5'
